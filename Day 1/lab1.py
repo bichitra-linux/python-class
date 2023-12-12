a3c8720639a46eb1a9b2a556d392c826722aae20
bichitra-Linux <bichitragautam25@gmail.com>
2023-12-12 20:15:40 +0545

    Add code to find the greatest number

---
+++ Day 1/lab1.py
@@ -0,0 +1,14 @@
+
+num1 = int(input("Enter the first number: "))
+num2 = int(input("Enter the second number: "))
+
+
+if num1 > num2:
+    greatest = num1
+else:
+    greatest = num2
+
+
+print("The greatest number is:", greatest)
+
+
