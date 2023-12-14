bb151667af2861282485f5bf0b24676515c375d0
bichitra-Linux <bichitragautam25@gmail.com>
2023-12-14 10:52:09 +0545

    Update user authentication logic

---
+++ day 2/2-3.py
@@ -0,0 +1,7 @@
+hours = int(input("Enter the number of hours: "))
+minutes = int(input("Enter the number of minutes: "))
+seconds = int(input("Enter the number of seconds: "))
+
+total_seconds = (hours * 3600) + (minutes * 60) + seconds
+
+print("Total seconds:", total_seconds)
