48f892cedecf7d60a3f25d09a9cb3837f801e99c
bichitra-Linux <bichitragautam25@gmail.com>
2023-12-15 19:42:33 +0545

    Add bird jumping and pipe spawning functionality

--- self/flappybird.py
+++ self/flappybird.py
@@ -1,4 +1,5 @@
 import pygame
+import random
 
 # Initialize pygame
 pygame.init()
@@ -12,10 +13,15 @@ pygame.display.set_caption("Flappy Bird")
 # Game variables
 gravity = 0.25
 bird_movement = 0
-bird_rect = pygame.Rect(100, 100, 50, 50)  # New bird rectangle
-
-
-# Load game assets (images, sounds, etc.)
+bird_jump = -12
+bird_rect = pygame.Rect(100, 100, 50, 50)
+ground_rect = pygame.Rect(0, window_height - 100, window_width, 100)
+pipe_width = 100
+pipe_height = random.randint(100, 400)
+pipe_gap = 200
+pipe_list = []
+SPAWNPIPE = pygame.USEREVENT
+pygame.time.set_timer(SPAWNPIPE, 1200)  # New timer event to spawn a new pipe every 1.2 seconds
 
 # Game loop
 running = True
@@ -24,16 +30,28 @@ while running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                bird_movement = bird_jump
+        if event.type == SPAWNPIPE:  # New event for spawning pipes
+            pipe_height = random.randint(100, 400)
+            bottom_pipe = pygame.Rect(window_width, window_height - pipe_height, pipe_width, pipe_height)
+            top_pipe = pygame.Rect(window_width, 0, pipe_width, window_height - pipe_height - pipe_gap)
+            pipe_list.extend((bottom_pipe, top_pipe))
 
-     # Update game logic
+    # Update game logic
     bird_movement += gravity
     bird_rect.y += bird_movement
+    pipe_list = [pipe.move(-5, 0) for pipe in pipe_list]  # Move the pipes to the left
 
     # Render game objects
-    window.fill((255, 255, 255))  # Fill the window with white
-    pygame.draw.rect(window, (255, 0, 0), bird_rect)  # Draw the bird
+    window.fill((255, 255, 255))
+    pygame.draw.rect(window, (255, 0, 0), bird_rect)
+    pygame.draw.rect(window, (0, 255, 0), ground_rect)
+    for pipe in pipe_list:  # Draw all the pipes
+        pygame.draw.rect(window, (0, 255, 0), pipe)
+
     # Update display
     pygame.display.update()
 
-# Clean up
 pygame.quit()
