0e445c9545169948a49c7f120a99629ea8e8d723
bichitra-Linux <bichitragautam25@gmail.com>
2023-12-15 19:38:55 +0545

    Add bird movement and rendering

--- self/flappybird.py
+++ self/flappybird.py
@@ -12,6 +12,8 @@ pygame.display.set_caption("Flappy Bird")
 # Game variables
 gravity = 0.25
 bird_movement = 0
+bird_rect = pygame.Rect(100, 100, 50, 50)  # New bird rectangle
+
 
 # Load game assets (images, sounds, etc.)
 
@@ -23,10 +25,13 @@ while running:
         if event.type == pygame.QUIT:
             running = False
 
-    # Update game logic
+     # Update game logic
+    bird_movement += gravity
+    bird_rect.y += bird_movement
 
     # Render game objects
-
+    window.fill((255, 255, 255))  # Fill the window with white
+    pygame.draw.rect(window, (255, 0, 0), bird_rect)  # Draw the bird
     # Update display
     pygame.display.update()
 
